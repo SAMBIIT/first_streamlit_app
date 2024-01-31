3b08a7d2b928d9e38a9465a64742df264068ff8e
SAMBIT SEKHAR SAHU <91898385+SAMBIIT@users.noreply.github.com>
2024-01-31 11:51:41 +0530

    Update streamlit_app.py
    added fruits list

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('3. 🐔 🍞 Hard-Boiled Free-Range Egg')
 streamlit.text('4. 🥑 Vitamin ABC Smothiee')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
